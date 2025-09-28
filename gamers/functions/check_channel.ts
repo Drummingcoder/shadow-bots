44c1ba9874ee4aca4f925170523e80592b771b2c
Tim Tran <121896621+Drummingcoder@users.noreply.github.com>
2025-09-28 23:36:24 +0000

    Added some quick features

--- gamers/functions/check_channel.ts
+++ gamers/functions/check_channel.ts
@@ -27,7 +27,112 @@ export const channelCheck = DefineFunction({
 export default SlackFunction(
   channelCheck,
   async ({ inputs, client }) => {
-    if ((inputs.message.includes("kys") || inputs.message.includes("kill yourself")) || (inputs.message.includes("leave"))) {
+    const myMessage = `${inputs.message}`;
+    if (inputs.message.includes("flip") && inputs.message.includes("coin")) {
+      const coin = Math.random();
+      let result = "";
+      if (coin < 0.5) {
+        result = "heads";
+      } else {
+        result = "tails";
+      }
+      await client.chat.postMessage({
+        channel: inputs.channel_id,
+        text: `It's ${result}!`,
+        thread_ts: inputs.timestamp,
+      });
+    } else if (inputs.message.includes("roll") && (inputs.message.includes("dice") || inputs.message.includes("die"))) {
+      const mess = myMessage.split(" ");
+      const num = parseInt(mess[2],10);
+      if (num == 0) {
+        await client.chat.postMessage({
+          channel: inputs.channel_id,
+          text: "You rolled nothing!",
+          thread_ts: inputs.timestamp,
+        });
+        return { outputs: {} };
+      }
+      await client.chat.postMessage({
+        channel: inputs.channel_id,
+        text: "Rolling...",
+        thread_ts: inputs.timestamp,
+      });
+      let str = "You rolled ";
+      if (num == 1) {
+        const num = Math.floor(Math.random() * (6) + 1);
+        str += "a " + num + ".";
+        await client.chat.postMessage({
+          channel: inputs.channel_id,
+          text: str,
+          thread_ts: inputs.timestamp,
+        });
+      } else {
+        let sum = 0;
+        const num1 = Math.floor(Math.random() * (6) + 1);
+        str += num1 + ", ";
+        sum += num1;
+        for (let i = 1; i < (num - 1); i++) {
+          const num2 = Math.floor(Math.random() * (6) + 1);
+          str += num2 + ", ";
+          sum += num2;
+        }
+        const num3 = Math.floor(Math.random() * (6) + 1);
+        str += "and " + num3 + ".";
+        sum += num3;
+        await client.chat.postMessage({
+          channel: inputs.channel_id,
+          text: str,
+          thread_ts: inputs.timestamp,
+        });
+        await client.chat.postMessage({
+          channel: inputs.channel_id,
+          text: "The sum of all rolled numbers is " + sum + ".",
+          thread_ts: inputs.timestamp,
+        });
+      }
+    } else if ((inputs.message.includes("eight") || inputs.message.includes("8")) && inputs.message.includes("ball")) {
+      await client.chat.postMessage({
+        channel: inputs.channel_id,
+        text: "Shaking the ball...",
+        thread_ts: inputs.timestamp,
+      });
+      let rando = Math.random(); //values from 0 to 0.99
+      let reroll = Math.random();
+      for (let chances = 0.4; reroll > chances; chances += 0.1) {
+        rando = Math.random();
+        reroll = Math.random();
+        await client.chat.postMessage({
+          channel: inputs.channel_id,
+          text: "Shaking the ball...",
+          thread_ts: inputs.timestamp,
+        });
+      }
+      if (rando < 0.4) {
+        const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "As I see it, yes", "Most likely", "Signs point to yes", "Outlook good"];
+        const index = Math.floor(Math.random() * (8));
+        await client.chat.postMessage({
+          channel: inputs.channel_id,
+          text: `The 8-ball says: "${responses[index]}"`,
+          thread_ts: inputs.timestamp,
+        });
+      } else if (rando < 0.7) {
+        const responses = ["Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful", "Highly Unlikely"];
+        const index = Math.floor(Math.random() * (6));
+        await client.chat.postMessage({
+          channel: inputs.channel_id,
+          text: `The 8-ball says: "${responses[index]}"`,
+          thread_ts: inputs.timestamp,
+        });
+      } else {
+        const responses = ["Reply hazy", "try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again"];
+        const index = Math.floor(Math.random() * (6));
+        await client.chat.postMessage({
+          channel: inputs.channel_id,
+          text: `The 8-ball says: "${responses[index]}"`,
+          thread_ts: inputs.timestamp,
+        });
+      }
+    } else if ((inputs.message.includes("kys") || inputs.message.includes("kill yourself")) || (inputs.message.includes("leave"))) {
       await client.conversations.leave({
         channel: inputs.channel_id,
       });
@@ -37,15 +142,13 @@ export default SlackFunction(
         thread_ts: inputs.timestamp,
       });
       return { outputs: { } };
-    }
-    const myMessage = `${inputs.message}`;
-    if (myMessage.includes("fuck") && ((myMessage.includes("u") || myMessage.includes("you")) || myMessage.includes("Dokeshi"))) {
+    } else if (myMessage.includes("fuck") && ((myMessage.includes("u") || myMessage.includes("you")) || myMessage.includes("Dokeshi"))) {
       await client.chat.postMessage({
         channel: inputs.channel_id,
         text: "You wanna do it with me? UwU",
         thread_ts: inputs.timestamp,
       });
-    } else if (((myMessage.includes("fun fact") || myMessage.includes("fun facts")) || (myMessage.includes("u") || myMessage.includes("you"))) || myMessage.includes("Dokeshi")) {
+    } else if ((myMessage.includes("fun fact") || myMessage.includes("fun facts")) && ((myMessage.includes("u") || myMessage.includes("you")) || myMessage.includes("Dokeshi"))) {
         await client.chat.postMessage({
           channel: inputs.channel_id,
           text: `Dokeshi is 6'7", has 420 friends, has parents that are 41 years old, and touched the hearts of 69 people!`,
@@ -57,9 +160,8 @@ export default SlackFunction(
         text: "Sorry, this response isn't supported yet.",
         thread_ts: inputs.timestamp,
       });
-      return { outputs: { error: "Please provide a supported message" } };
+      return { outputs: { error: "Can't handle this response" } };
     }
-
     return { outputs: {} };
   },
 );
