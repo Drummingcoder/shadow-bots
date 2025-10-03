a94652bfc78ecf624db5c501f21b047c8cb0ca80
Tim Tran <121896621+Drummingcoder@users.noreply.github.com>
2025-10-03 22:20:56 +0000

    more docs

--- sans/manifest.ts
+++ sans/manifest.ts
@@ -11,7 +11,7 @@ import minusCoins from "./datastores/coinusers.ts";
 export default Manifest({
   name: "Sans Tracker",
   description: "Spending a bit too much time on Slack, are we?",
-  longDescription: `This bot helps to track your time on Slack and ping you for every 30 minutes you spend on Slack in a day. For every hour you spend off of Slack (minus 7 hours for sleep time), the bot will give you 1 coin (at 12:00am PST). You can use one coin to skip one ping using the '/timetospend' command. To start tracking, simply just mention the bot in a channel that it's in (no other message is needed) and follow the prompts. \n\nThis bot only operates in <#C09GDF8ETQB>, so join it if you want to start tracking your time. \n\nFor more details, go to https://hackclub.slack.com/docs/T0266FRGM/F09JPH7HY82.`,
+  longDescription: `This bot helps to track your time on Slack and ping you for every 30 minutes you spend on Slack in a day. For every hour you spend off of Slack (minus 7 hours for sleep time), the bot will give you 1 coin (at 12:00am PST). You can use one coin to skip one ping using the '/timetospend' command. To start tracking, simply just mention the bot in a channel that it's in (no other message is needed) and follow the prompts. \n\nThis bot only operates in <#C09GDF8ETQB>, so join it if you want to start tracking your time. \n\nFor more details, go to https://hackclub.slack.com/docs/T0266FRGM/F09JPH7HY82, or just ask questions in <#C09GDF8ETQB>.`,
   icon: "assets/sans.jpg",
   workflows: [responder, first, checke, checked, spent],
   outgoingDomains: [],
