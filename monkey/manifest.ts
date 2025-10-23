2e892acf9a21ad129b6a9a429492a48fe4d8626c
Tim Tran <121896621+Drummingcoder@users.noreply.github.com>
2025-10-23 17:09:52 +0000

    Added scopes and deployed bot

--- monkey/manifest.ts
+++ monkey/manifest.ts
@@ -1,19 +1,14 @@
 import { Manifest } from "deno-slack-sdk/mod.ts";
 import SampleWorkflow from "./workflows/sample_workflow.ts";
 import SampleObjectDatastore from "./datastores/sample_datastore.ts";
+import keepgoing from "./workflows/joining.ts";
 
 export default Manifest({
   name: "Monkey",
   description: "Ooh ooh ah ah",
   icon: "assets/monkey.jpg",
-  workflows: [SampleWorkflow],
+  workflows: [SampleWorkflow, keepgoing],
   outgoingDomains: [],
   datastores: [SampleObjectDatastore],
-  botScopes: [
-    "commands",
-    "chat:write",
-    "chat:write.public",
-    "datastore:read",
-    "datastore:write",
-  ],
+  botScopes: [ "channels:history", "im:history", "groups:history", "mpim:history", "commands", "chat:write", "chat:write.public", "datastore:read", "datastore:write", "app_mentions:read", "channels:read", "groups:read", "im:read", "mpim:read", "channels:join", "channels:manage", "channels:write.invites", "groups:write", "groups:write.invites", "im:write", "mpim:write"],
 });
